5903197440c05d1b6baa4c4f6d55fc0d9880117b
TheAmitBarua <89492966+TheAmitBarua@users.noreply.github.com>
2022-12-17 15:11:27 -0800

    Add files via upload

---
+++ Group.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.tree.DefaultMutableTreeNode;
+
+
+public class Group implements Tree {
+    private long creationTime;
+	private boolean target;
+    private String SSID;
+    private List<User> GroupGroupUsers = new ArrayList();
+    private static DefaultMutableTreeNode root;
+
+    private int GroupCount = 0;
+
+     public Group() {
+    	root = new DefaultMutableTreeNode("Root");
+        GroupCount++;
+        this.creationTime = System.currentTimeMillis()
+    }
+     public Group(String SSID) {
+        this(SSID, true);
+        GroupCount++;
+    }
+     public Group(String SSID, boolean property) {
+        this.target = property;
+        this.SSID = SSID;
+        GroupCount++;
+    }
+
+    public int getgroupCount() {
+        return GroupCount;
+    }
+
+    public DefaultMutableTreeNode getRoot() {
+        return root;
+    }
+
+    @Override
+    public String getID() {
+        return SSID;
+    }
+
+    @Override
+    public boolean isResult() {
+        return target;
+    }
+    
+    @Override
+    public String toString() {
+        return SSID;
+    }
+
+    
+    public long getCreationTime() {
+        return creationTime;
+    }
+    
+    public void setCreationTime(long creationTime) {
+        this.creationTime = creationTime;
+    }
+    
+}
---
+++ driver.java
@@ -0,0 +1,8 @@
+
+public class driver {
+    
+    public static void main(String[] args) {
+        Singleton.getInstance().setVisible(true);
+
+    }
+}
